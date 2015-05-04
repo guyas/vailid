bf9f5bffafb488d8ef92addd2e7ffe9bfa0edde2
guyas <guy.ashiri@gmail.com>
2015-05-04 15:07:17 +0300

    2nd commit, code inside

--- lib/vailid.rb
+++ lib/vailid.rb
@@ -1,5 +1,30 @@
 require "vailid/version"
 
 module Vailid
-  # Your code goes here...
+	
+	LENGTH = 9 # Accepted maximal valid length for IL id number
+	
+	def initialize
+	
+	end
+	
+	def self.valid?(str)
+		return false unless (str != null)
+		return false unless (str.length <= LENGTH)
+		while(str.length < LENGTH)
+			str="0".concat(str)
+		end
+		sum=0
+		for i in 0..(LENGTH-1)
+			if (i%2==0)
+				sum+=	str[i]
+			else
+				temp= 2*str[i]
+				(temp=temp/10 + temp%10) unless (temp<10)
+				sum+=temp
+			end
+		end
+		(sum%10==0)? true : false
+	end
+	
 end
---
+++ lib/vailid/version.rb~
@@ -0,0 +1,3 @@
+module Vailid
+  VERSION = "0.0.1"
+end
